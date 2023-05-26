82c8555b98af97191aa1416fdfd439770298654b
Jonathon Day <45298953+jon0695@users.noreply.github.com>
2023-05-25 23:16:43 -0400

    Updated quote generator
    Fixed up the css, reordered the html, and changed up the js.

--- Javascript starter projects/project3/webpage/script.js
+++ Javascript starter projects/project3/webpage/script.js
@@ -1,14 +1,27 @@
 
 const returnJSONData = async (fileQuery) => {
-    const response = await fetch(fileQuery);
-    const jsonData = await response.json();
-    return jsonData;
-  }
-
-const changeQuote = async () => {
-    const quotes = Object.values(await returnJSONData("./famousQuotes.json"));
-    randomNumber = Math.floor(Math.random() * quotes.length);
-    document.getElementById('quoteContainer').textContent = quotes[randomNumber];
+  const response = await fetch(fileQuery);
+  const jsonData = await response.json();
+  return jsonData;
 }
 
-changeQuote();
+let oldRandomNumber = NaN;
+const changeQuote = async () => {
+  const quotes = Object.values(await returnJSONData("./famousQuotes.json"));
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  if (oldRandomNumber !== randomNumber) {
+    splitQuote = splitQuoteAndAuthor(quotes[randomNumber]);
+    document.getElementById('quoteP').textContent = splitQuote[0];
+    document.getElementById('author').textContent = (" - " + splitQuote[1]);
+    oldRandomNumber = randomNumber;
+  }else{
+    console.log("Would have used the same quote twice. So i called the function again.");
+    changeQuote();
+  }
+}
+
+const splitQuoteAndAuthor = (quoteData) => {
+  const newData = quoteData.split("~~");
+  console.log(newData);
+  return newData;
+}
